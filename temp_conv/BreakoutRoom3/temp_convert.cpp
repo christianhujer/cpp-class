8d8001252f615987f0e902cb4ea716eecd1d9729
kshitij-jadhav <kshitij.jadhav@cctech.co.in>
2022-05-25 13:27:46 +0530

    added getScale function.

--- temp_conv/BreakoutRoom3/temp_convert.cpp
+++ temp_conv/BreakoutRoom3/temp_convert.cpp
@@ -42,37 +42,71 @@ double FahrenheitConvertor ::fromKelvin(double kelvin)
     return 1.8 * (kelvin - 273) + 32;
 }
 
-double FahrenheitConvertor ::toKelvin(double fahrenheit)
+double FahrenheitConvertor :: toKelvin(double fahrenheit)
 {
     return (0.55555 * (fahrenheit - 32)) + 273.15;
 }
 
 
-bool KelvinConvertor ::isScale(string inputScale) {
+bool KelvinConvertor :: isScale(string inputScale) {
     list<string> celsiusScales{"k","K", "kelvin", "Kelvin"};
     return isInScale(inputScale,celsiusScales); 
 }
 
-double KelvinConvertor ::fromKelvin(double kelvin)
+double KelvinConvertor :: fromKelvin(double kelvin)
 {
     return kelvin;
 }
 
-double KelvinConvertor ::toKelvin(double kelvin)
+double KelvinConvertor :: toKelvin(double kelvin)
 {
     return kelvin;
 }
-/*TemperatureConvertor TemperatureConvertor::getToScale(string inputScale){
-    
+TemperatureConvertor* getScale(string inputScale){
+    CelsiusConvertor celsius;
+    KelvinConvertor kelvin;
+    FahrenheitConvertor fahrenheit;
+    TemperatureConvertor *temp;
+
+    if (celsius.isScale(inputScale)) {
+        temp = &celsius;
+    }
+
+    else if (kelvin.isScale(inputScale)) {
+        temp = &kelvin;
+    }
+
+    else if (fahrenheit.isScale(inputScale)) {
+        temp = &fahrenheit;
+    }
+
+    else {
+        cerr << "error:Unsupported temprature scale." << inputScale << endl;
+        abort();
+    }
+    return temp;
+}
+
+/*void runTest(){
+    assert(toScale->fromKelvin(0) == -273.15);
+    assert(toScale->isScale("C"));
 }*/
 
-int main()
+int main(int argc, char* argv[])
 {
-    TemperatureConvertor *fromScale, *toScale;
+    if (argc < 3) {
+        cerr << argv[0] << ": error: Not enough arguments.\n"
+        "Usage: temp_convert INPUT_SCALE OUTPUT_SCALE [TEMPERATURE]...";
+    }
+    TemperatureConvertor *inputScale, *outputScale;
     CelsiusConvertor celsius;
-    toScale = &celsius;
-    //toScale = getToScale("C");    
-    assert(toScale->fromKelvin(0) == -273.15);
-    assert(toScale->isScale("C") == true);
+    inputScale = getScale(argv[1]);
+    outputScale = getScale(argv[2]);
+    for (int i = 3; i < argc; i++) {
+    int inputTemperature = atof(argv[i]);
+    int tempInKelvin = inputScale->toKelvin(inputTemperature);
+    int outputTemperature =outputScale->fromKelvin(tempInKelvin);
+    cout << outputTemperature << endl;
+    }
     return EXIT_SUCCESS;
 }
