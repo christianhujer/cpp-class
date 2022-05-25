1c4b655aed34b4cea1d12ab0423653bcfee5e8b7
Pratik <pratikpadman125@gmail.com>
2022-05-25 11:33:24 -0700

    First working version

---
+++ temp_conv/BreakoutRoom2/Temp_Convert.h
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+class TemperatureConverter
+{
+public:
+    virtual double fromKelvin(double Kelvin) = 0;
+    virtual double toKelvin(double Other) = 0;
+    virtual bool isSupportingScale(string inputScale) = 0;
+    bool isInTempStrings(string inputScale, list<string> outputScale);
+};
+
+class CelsiusConverter : public TemperatureConverter
+{
+public:
+    double fromKelvin(double Kelvin);
+    double toKelvin(double Other);
+    bool isSupportingScale(string inputScale);
+};
+
+class FahrenheitConverter : public TemperatureConverter
+{
+public:
+    double fromKelvin(double Kelvin);
+    double toKelvin(double Other);
+    bool isSupportingScale(string inputScale);
+};
+
+class KelvinConverter : public TemperatureConverter
+{
+public:
+    double fromKelvin(double Kelvin);
+    double toKelvin(double Other);
+    bool isSupportingScale(string inputScale);
+};
