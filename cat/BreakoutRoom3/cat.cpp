d68935a35218d6e1b9f102a9e6cc4574b2a9a2b0
Pratik <pratikpadman125@gmail.com>
2022-05-29 13:39:22 -0700

    Added new file

---
+++ cat/BreakoutRoom3/cat.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+void concatenateFileContent(int argc, string filename)
+{
+    ifstream myfile(filename.c_str());
+    if (myfile.is_open())
+    {
+        cout << myfile.rdbuf();
+    }
+    else
+    {
+        cerr << filename << " does not exist";
+    }
+    myfile.close();
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc == 1)
+    {
+        cout << cin.rdbuf();
+    }
+    else
+    {
+        for (int i = 1; i < argc; i++)
+        {
+            string inputFileName = argv[i];
+            concatenateFileContent(argc, inputFileName);
+        }
+    }
+    return EXIT_SUCCESS;
+}
