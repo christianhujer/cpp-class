dd1349c547acc017b45d6089b6223dd775110522
kshitij-jadhav <kshitij.jadhav@cctech.co.in>
2022-05-31 11:23:41 +0530

    modified echo.cpp.

--- echo/BreakoutRoom1/echo.cpp
+++ echo/BreakoutRoom1/echo.cpp
@@ -4,16 +4,18 @@
 
 using namespace std;
 
+string echo(int argc, char *argv[]) {
+    stringstream ss;
+    for (int i = 1; i < argc; i++) {
+        if (i > 1) 
+            ss << " ";
+        ss << argv[i];
+    }
+    ss << endl;
+    return ss.str();
+}
+
 int main(int argc, char* argv[]) {
-    stringstream s;
-    for (int i = 1; i < argc; i++)
-    {
-        if (i == (argc - 1))
-            s << argv[i];
-        else {
-            s << argv[i] << " ";
-        }
-    }  
-    cout << s.str();
+    cout << echo(argc, argv);
     return EXIT_SUCCESS;
 }
