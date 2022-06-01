f475aee4b5790dd6c201748d5a981632d13c20a6
kshitij-jadhav <kshitij.jadhav@cctech.co.in>
2022-06-01 13:10:40 +0530

    working version of isPalindrome.c file.

--- palindrome/BreakoutRoom1/isPalindrome.c
+++ palindrome/BreakoutRoom1/isPalindrome.c
@@ -7,11 +7,11 @@
 
 char *removeNoise(char *word) {
     
-    int len = 0;  
-    len = sizeof(word)/sizeof(word[0]);
-    for (int i = 0; i < len; i++){
+    int len = 0;
+    len = strlen(word);
+    for (int i = 0; i < len; i++) {
         word[i] = tolower(word[i]);
-    }   
+    }
     for (int i = 0; i < len; i++){  
         if (word[i] == ' '){  
             for (int j = i; j < len; j++)  
@@ -21,26 +21,41 @@ char *removeNoise(char *word) {
         len--;  
         }  
     }
-    printf("%s\n",word);
     return word;
 }
 
 char* invert(char *word) {
-    char *invertedWord;
+    char *invertedWord = malloc((size_t) 30);
+    int len = strlen(word) - 1;
+    for (int i = 0; i < strlen(word); i++) {
+        invertedWord[i] = word[len--]; 
+    }
+    invertedWord[strlen(word)] = '\0';
+    return invertedWord;
 }
 
 bool isPlaindrome(char *word) {
     word = removeNoise(word);
-    return word == invert(word);
+    if (strcmp(word,invert(word)) == 0) {
+        printf("%s is a palindrome\n",word);
+        return true;
+    }
+    else {
+         printf("%s is not a palindrome\n",word);
+         return false;
+    }
 }
 
 void test() {
-    assert(strcmp(removeNoise("Amok Oma"),"amokoma"));
-    assert(strcmp(removeNoise("Amok Oma"),"amo ikoma"));
+    char *testWord = malloc((size_t) 100);
+    strcpy(testWord,"Amok oma");
+    assert(strcmp(removeNoise(testWord),"amokoma") == 0);
+    assert(strcmp(invert(testWord),"amokoma") == 0);
 }
 
 int main(int argc, char *argv[]) {
     test();
+ 
     bool status = EXIT_SUCCESS;
     for (int i = 1; i < argc; i++) {
         if (!isPlaindrome(argv[i])) {
