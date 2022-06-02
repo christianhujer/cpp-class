3ba0a92c1049ba8bf186812810b613a4b1f1bbb6
Pratik <pratikpadman125@gmail.com>
2022-06-02 12:52:18 -0700

    BreakoutRoom1

--- braceBalance/BreakoutRoom1/braceBalance.cpp
+++ braceBalance/BreakoutRoom1/braceBalance.cpp
@@ -1,30 +1,61 @@
 #include <iostream>
 #include <stdbool.h>
-#include <vector>
 #include <fstream>
 #include <string>
+#include <stack>
 using namespace std;
 
-bool isBalanced(int argc, string inputfile)
+bool Pair(char left, char right)
 {
-     vector<string> vectorBraces;
-     ifstream newfile(inputfile);
+    if (left == '(' && right == ')')
+        return true;
+    else if (left == '{' && right == '}')
+        return true;
+    else if (left == '[' && right == ']')
+        return true;
+    return false;
+}
 
-     if(newfile.is_open())
-     {
-         string line;
-         while(getline(newfile, line))
-         {
-             
-         }
-     }
+bool isBalanced(string inputfile)
+{
+    stack<char> S;
+    ifstream newfile(inputfile);
 
+    if (newfile.is_open())
+    {
+        string line;
+        while (getline(newfile, line))
+        {
 
+            for (int i = 0; i < line.length(); i++)
+            {
+                if (line[i] == '(' || line[i] == '{' || line[i] == '[')
+                    S.push(line[i]);
+                else if (line[i] == ')' || line[i] == '}' || line[i] == ']')
+                {
+                    if (S.empty() || !Pair(S.top(), line[i]))
+                        return false;
+                    else
+                        S.pop();
+                }
+            }
+        }
+    }
+    return S.empty() ? true : false;
+}
 
-};
-
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     string inputfile = argv[1];
-    isBalanced(argc, inputfile); 
+    bool exitStatus = EXIT_SUCCESS;
+    if (isBalanced(inputfile))
+    {
+        cout<<"Balanced";
+    }
+    else{
+        cerr<<"error: missing ";
+        exitStatus = EXIT_FAILURE;
+    }
+    return exitStatus;
+
 }
