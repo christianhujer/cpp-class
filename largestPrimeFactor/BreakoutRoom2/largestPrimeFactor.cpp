2e573560000dc0f609a1b56c4da29bafc2b4b5d7
Pratik <pratikpadman125@gmail.com>
2022-06-02 12:09:13 -0700

    Added new file

---
+++ largestPrimeFactor/BreakoutRoom2/largestPrimeFactor.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+bool isprime(int factor){
+    for (int i = 2; i <= factor/2; i++)
+    {
+        if(factor % i != 0)
+            continue;
+        else
+        return false;
+    }
+    return true;
+}
+
+long long largestPrimeFactor(long long inputNumber) {
+    long long largestPrimeFactor = 0;
+    for (int factor = 2; inputNumber != 1; factor++) {
+        if(inputNumber % factor == 0 && isprime(factor)){
+            inputNumber /= factor;
+            largestPrimeFactor = factor;
+        }
+    }
+    return largestPrimeFactor;
+}
+
+int main() {
+    long long inputNumber = 600851475143;
+    cout << "Largest prime factor of " << inputNumber <<" is: " << largestPrimeFactor(inputNumber) << endl;
+    return EXIT_SUCCESS;
+}
