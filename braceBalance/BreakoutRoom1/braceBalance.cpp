0a9e5b13a53f99c473eb4c00d9c45f89219a3297
shawmonika616ms <shawmonika616ms@gmail.com>
2022-06-03 11:31:59 +0530

    BreakoutRoom1

--- braceBalance/BreakoutRoom1/braceBalance.cpp
+++ braceBalance/BreakoutRoom1/braceBalance.cpp
@@ -1,30 +1,40 @@
-#include <iostream>
-#include <stdbool.h>
-#include <vector>
-#include <fstream>
-#include <string>
+#include<iostream>
+#include<stack>
+#include<string>
 using namespace std;
 
-bool isBalanced(int argc, string inputfile)
+bool Pair(char left,char right)
 {
-     vector<string> vectorBraces;
-     ifstream newfile(inputfile);
-
-     if(newfile.is_open())
-     {
-         string line;
-         while(getline(newfile, line))
-         {
-             
-         }
-     }
-
-
-
-};
-
-int main(int argc, char* argv[])
+    if(left == '(' && right == ')') return true;
+    else if(left == '{' && right == '}') return true;
+    else if(left == '[' && right == ']') return true;
+    return false;
+}
+bool isBalanced(string strings)
 {
-    string inputfile = argv[1];
-    isBalanced(argc, inputfile); 
+    stack<char>  S;
+    for(int i =0;i<strings.length();i++)
+    {
+        if(strings[i] == '(' || strings[i] == '{' || strings[i] == '[')
+            S.push(strings[i]);
+        else if(strings[i] == ')' || strings[i] == '}' || strings[i] == ']')
+        {
+            if(S.empty() || !Pair(S.top(),strings[i]))
+                return false;
+            else
+                S.pop();
+        }
+    }
+    return S.empty() ? true:false;
+}
+
+int main()
+{
+    string str;
+    cout<<"Enter a brace:  ";
+    cin>>str;
+    if(isBalanced(str))
+        cout<<"Yes, balanced\n"<<str;
+    else
+        cout<<"Not Balanced\n"<<str;
 }
