dcc97b3ff74639af6153c505c89a4b8ef1615abe
Pratik <pratikpadman125@gmail.com>
2022-06-05 12:29:38 -0700

    refactor smallestMultiple.cpp file

--- smallestMultiple/BreakoutRoom1/smallestMultiple.cpp
+++ smallestMultiple/BreakoutRoom1/smallestMultiple.cpp
@@ -2,30 +2,29 @@
 
 using namespace std;
 
-int lcmOfTwo(int x, int y)
+int smallestMultiple()
 {
-    int max;
-    max = (x > y) ? x : y;
-
-    while (true)
+    int result;
+    for (int i = 20; ; i++)
     {
-        if ((max % x == 0) && (max % y == 0))
-            return max;
-        ++max;
+        result = 0;
+        for (int j = 1; j < 21; j++)
+        {
+            if (i % j == 0)
+            {
+                result++;
+            }
+        }
+        if (result == 20)
+        {
+            return i;
+            break;
+        }
     }
-}
-
-int smallestMultiple(int range[])
-{
-    int result = range[0];
-    for (int i = 0; i < 20; i++)
-        result = lcmOfTwo(result, range[i]);
-    return result;
 }
 
 int main(void)
 {
-    int numberRange[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
-    cout << smallestMultiple(numberRange) << endl;
+    cout << smallestMultiple() << endl;
     return EXIT_SUCCESS;
 }
